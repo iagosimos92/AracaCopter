4726b5d49760c1a8f60c450518e24a90d08f75a5
Iago <iagosimoes92@gmail.com>
2018-03-09 18:19:36 -0300

    Update MotionSensor.h

--- MotionSensor.h
+++ MotionSensor.h
@@ -9,8 +9,6 @@
 extern float ypr[3]; //yaw, pitch, roll
 extern float accel[3];
 extern float gyro[3];
-extern float temp;
-extern float compass[3];
 
 extern int ms_open();
 extern int ms_update();
