862a7c778098c14252825040a2c0acc0fc3bc4f8
Iago <iagosimoes92@gmail.com>
2018-03-09 18:22:37 -0300

    Update main.c

--- main.c
+++ main.c
@@ -11,10 +11,7 @@ int main() {
 	ms_open();
 	do{
 		ms_update();
-		printf("yaw = %2.1f\tpitch = %2.1f\troll = %2.1f\ttemperature = %2.1f\tcompass = %2.1f, %2.1f, %2.1f\n",
-		 ypr[YAW], ypr[PITCH],
-		 ypr[ROLL],temp,compass[0],compass[1],compass[2]);
-		delay_ms(5);
+		printf("yaw = %2.1f\tpitch = %2.1f\troll = %2.1f\n",ypr[YAW], ypr[PITCH],ypr[ROLL]);
 	}while(1);
 
 	return 0;
