a7f2aba51bcfaa8daa1b104a8dc9b16d4d90cbfe
Iago <iagosimoes92@gmail.com>
2018-03-12 13:47:35 -0300

    Delete servo.py

--- ServoBlaster/user/servo.py
+++
@@ -1,7 +0,0 @@
-# use file to set a servo position in us
-def servo_set_us(servo, pos):
-    with open("/dev/servoblaster", 'w') as fd:
-        cmd = str(servo) + '=' + str(pos) + 'us'
-        cmd += '\n'
-        fd.write(cmd)
-servo_set_us(0,1000)
