9b9ca70fa4aecc1acce3f7a9b3dee9bb055c45fa
Iago <iagosimoes92@gmail.com>
2018-03-13 13:57:41 -0300

    Update greetmodule.c

--- greetmodule.c
+++ greetmodule.c
@@ -3,15 +3,10 @@
 static PyObject *
 greet_name(PyObject *self, PyObject *args)
 {    
-    printf("Hello %s!\n", name);
+    printf("Hello\n");
 
     Py_RETURN_NONE;
 }
-
-static PyMethodDef GreetMethods[] = {
-    {"greet", greet_name, METH_VARARGS, "Greet an entity."},
-    {NULL, NULL, 0, NULL}
-};
 
 PyMODINIT_FUNC
 initgreet(void)
