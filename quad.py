87c34d1063a17704e8f2fd4e01b27fa6576c8e28
Iago <iagosimoes92@gmail.com>
2018-03-13 15:19:42 -0300

    Update quad.py

--- quad.py
+++ quad.py
@@ -2,7 +2,5 @@ from ctypes import *
 
 #load the shared object file
 adder = CDLL('./adder.so')
-
 add_float = adder.add_float
-add_float.restype = c_float
 print "Sum 3.2 + 2.1 = ", str(add_float())
