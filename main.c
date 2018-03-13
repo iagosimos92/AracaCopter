6e36f7b2b26533fbfd512ea3e531634992b6c803
Iago <iagosimoes92@gmail.com>
2018-03-13 15:06:44 -0300

    Update main.c

--- main.c
+++ main.c
@@ -7,11 +7,16 @@
 
 #define delay_ms(a) usleep(a*1000)
 
+float add_float();
+
 int main() {
 	ms_open();
 	do{
 		ms_update();
-		printf("yaw = %2.1f\tpitch = %2.1f\troll = %2.1f\n",ypr[YAW], ypr[PITCH],ypr[ROLL]);
+		//printf("yaw = %2.1f\tpitch = %2.1f\troll = %2.1f\n",ypr[YAW], ypr[PITCH],ypr[ROLL]);
+		float add_float(){
+		    return ypr[YAW];
+		}
 	}while(1);
 
 	return 0;
