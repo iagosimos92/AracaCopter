5181019486c1b1de8ad9bf7ad30619644779ad24
Iago <iagosimoes92@gmail.com>
2018-03-13 15:11:50 -0300

    Update main.c

--- main.c
+++ main.c
@@ -9,14 +9,12 @@
 
 float add_float();
 
-int main() {
+float add_float() {
 	ms_open();
 	do{
 		ms_update();
 		//printf("yaw = %2.1f\tpitch = %2.1f\troll = %2.1f\n",ypr[YAW], ypr[PITCH],ypr[ROLL]);
-		float add_float(){
-		    return ypr[YAW];
-		}
+		return ypr[YAW];
 	}while(1);
 
 	return 0;
