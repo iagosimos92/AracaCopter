dd13f0bc45e7e30ec775c3564f000bda61e572b2
Iago <iagosimoes92@gmail.com>
2018-03-13 15:33:46 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,11 +1,19 @@
+#include <unistd.h>
 #include <stdio.h>
+#include <math.h>
+#include <time.h>
+
 #include "MotionSensor.h"
+
+#define delay_ms(a) usleep(a*1000)
 
 int main() {
 	ms_open();
-	while(1){
+	do{
 		ms_update();
 		printf("yaw = %2.1f\tpitch = %2.1f\troll = %2.1f\n",ypr[YAW], ypr[PITCH],ypr[ROLL]);
-	}
+	}while(1);
+
 	return 0;
 }
+
