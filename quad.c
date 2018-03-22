4a4e29a0175ecdee2fac3d644e3d785cddbb29b6
Iago <iagosimoes92@gmail.com>
2018-03-22 09:18:26 -0300

    Update quad.c

--- quad.c
+++ quad.c
@@ -21,6 +21,9 @@ int main()
     system("echo 0=2000us > /dev/servoblaster");//go to 180 degree
     sleep(1);
       
+    system("echo 0=1500us > /dev/servoblaster");//go to 90 degree
+    sleep(1);
+      
     ms_update();
     printf("yaw = %2.1f\tpitch = %2.1f\troll = %2.1f\n",ypr[YAW], ypr[PITCH],ypr[ROLL]);
     }
