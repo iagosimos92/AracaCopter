9fbad743791153a04834d08ca140dfc52ec7057f
Iago <iagosimoes92@gmail.com>
2018-03-24 17:59:16 -0300

    Update p.c

--- p.c
+++ p.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
 #include <wiringPi.h>
 
-pinMode (18, INPUT) ; 
-GPIO.add_event_detect(18, GPIO.FALLING, callback_exit);
-//printf("Saindo do programa.");
+void main (){
 
-while(1){}
-
-void callback_exit(){
+	pinMode (18, INPUT) ; 
+	GPIO.add_event_detect(18, GPIO.FALLING, callback_exit);
 	//printf("Saindo do programa.");
+
+	while(1){}
+
+	void callback_exit(){
+		//printf("Saindo do programa.");
+	}
 }
