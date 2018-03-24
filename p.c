44470d6bcdfa8292b80a713019e5b2ff56ab8602
Iago <iagosimoes92@gmail.com>
2018-03-24 17:51:31 -0300

    Update p.c

--- p.c
+++ p.c
@@ -3,21 +3,21 @@
 
 void init_env(){
   GPIO.setmode(GPIO.BOARD);
-	GPIO.setwarnings(False);
+  GPIO.setwarnings(False);
 }
 
 void init_pin(){
-	GPIO.setup(18,GPIO.IN);
+  GPIO.setup(18,GPIO.IN);
 }
 
 int callback_exit(channel){
-	printf("Saindo do programa.");
+	//printf("Saindo do programa.");
 }
 
 init_env();
 init_pin();
 
 GPIO.add_event_detect(18, GPIO.FALLING, callback_exit);
-printf("Saindo do programa.");
+//printf("Saindo do programa.");
 
 while(1){}
