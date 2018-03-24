835809e2ead37ffb728fc9d898d272c214562427
Iago <iagosimoes92@gmail.com>
2018-03-24 17:53:49 -0300

    Update p.c

--- p.c
+++ p.c
@@ -1,23 +1,12 @@
 #include <stdio.h>
 #include <wiringPi.h>
 
-void init_env(){
-  GPIO.setmode(GPIO.BOARD);
-  GPIO.setwarnings(False);
-}
-
-void init_pin(){
-  GPIO.setup(18,GPIO.IN);
-}
-
-int callback_exit(channel){
-	//printf("Saindo do programa.");
-}
-
-init_env();
-init_pin();
-
+GPIO.setup(18,GPIO.IN);
 GPIO.add_event_detect(18, GPIO.FALLING, callback_exit);
 //printf("Saindo do programa.");
 
 while(1){}
+
+void callback_exit(){
+	//printf("Saindo do programa.");
+}
