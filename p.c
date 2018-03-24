83f42417f7e67ffb7415cac4386927687546210f
Iago <iagosimoes92@gmail.com>
2018-03-24 17:57:57 -0300

    Update p.c

--- p.c
+++ p.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <wiringPi.h>
 
-GPIO.setup(18,GPIO.IN);
+pinMode (18, INPUT) ; 
 GPIO.add_event_detect(18, GPIO.FALLING, callback_exit);
 //printf("Saindo do programa.");
 
