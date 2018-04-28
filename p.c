7b0cf3dd95ffda5de42538f4025cc80b3c340e90
Iago <iagosimoes92@gmail.com>
2018-04-28 16:51:13 -0300

    Update p.c

--- p.c
+++ p.c
@@ -2,8 +2,6 @@
 #include <sys/time.h>
 #include <wiringPi.h>
 
-
-
 void handle() {
 	printf("Rising\n");
 }
@@ -11,7 +9,7 @@ void handle() {
 int main() {
 	// Init
 	wiringPiSetup();
-	pinMode(18, INPUT);
+	pinMode(18, OUTPUT);
 	wiringPiISR(18, INT_EDGE_RISING, handle);
 	
 	while(1){
