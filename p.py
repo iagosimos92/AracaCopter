174c1f258bb655bc39d80cfa9a3a0e96ebd8339e
Iago <iagosimoes92@gmail.com>
2018-04-28 17:25:59 -0300

    Update p.py

--- p.py
+++ p.py
@@ -2,17 +2,30 @@ from os import system
 from time import sleep
 import RPi.GPIO as GPIO
 
-
-def callback_exit(channel):
-	print("ISR")
-
-
-def init():
-	
+def init_env():
 	GPIO.setmode(GPIO.BOARD)
 	GPIO.setwarnings(False)
+
+def init_pin():
 	GPIO.setup(18,GPIO.IN)
-	GPIO.add_event_detect(18, GPIO.FALLING, callback_exit)
 
-while(1):
 
+
+
+def callback_exit(channel):
+	print("Botão pressionado no pino ",channel)
+
+
+###################
+# Inicio programa #
+###################
+
+init_env()
+init_pin()
+
+GPIO.add_event_detect(18, GPIO.FALLING, callback_exit)
+
+
+loop=True
+while(loop):
+	loop=True
