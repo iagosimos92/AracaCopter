a88e8e2a6d9285841a7631a6f911e6eac995ea1a
Iago <iagosimoes92@gmail.com>
2018-04-28 16:57:33 -0300

    Update p.c

--- p.c
+++ p.c
@@ -12,7 +12,7 @@ int main() {
 	wiringPiISR(18, INT_EDGE_RISING, &handle);
 	if (wiringPiSetup () < 0)
 	  {
-	    fprintf ( "Unable to setup wiringPi: \n") ;
+	    printf ( "Unable to setup wiringPi: \n") ;
 	    return 1 ;
 	  }
 	while(1){
