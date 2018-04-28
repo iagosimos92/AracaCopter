da68c823b87093660cd5f306c9bc61ecb3a49d03
Iago <iagosimoes92@gmail.com>
2018-04-28 16:59:52 -0300

    Update p.c

--- p.c
+++ p.c
@@ -1,21 +1,17 @@
 #include <stdio.h>
 #include <sys/time.h>
 #include <wiringPi.h>
-
+int i=0;
 void handle(void) {
-	printf("Rising\n");
+	i++;
 }
 
 int main() {
 	// Init
 	wiringPiSetup();
 	wiringPiISR(18, INT_EDGE_RISING, &handle);
-	if (wiringPiSetup () < 0)
-	  {
-	    printf ( "Unable to setup wiringPi: \n") ;
-	    return 1 ;
-	  }
+	
 	while(1){
-		
+		printf("%d",i);
 	}
 }
