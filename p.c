e58b1115f3cf778a81e40096045dad8f239095f4
Iago <iagosimoes92@gmail.com>
2018-04-28 16:14:33 -0300

    Update p.c

--- p.c
+++ p.c
@@ -1,15 +1,67 @@
 #include <stdio.h>
+#include <time.h>
 #include <wiringPi.h>
 
-void main (){
+// Which GPIO pin we're using
+#define PIN 2
+// How much time a change must be since the last in order to count as a change
+#define IGNORE_CHANGE_BELOW_USEC 10000
 
-	pinMode (18, INPUT) ; 
-	GPIO.add_event_detect(18, GPIO.FALLING, callback_exit);
-	//printf("Saindo do programa.");
+// Current state of the pin
+static volatile int state;
+// Time of last change
+struct timeval last_change;
 
-	while(1){}
+// Handler for interrupt
+void handle(void) {
+	struct timeval now;
+	unsigned long diff;
 
-	void callback_exit(){
-		//printf("Saindo do programa.");
+	gettimeofday(&now, NULL);
+
+	// Time difference in usec
+	diff = (now.tv_sec * 1000000 + now.tv_usec) - (last_change.tv_sec * 1000000 + last_change.tv_usec);
+
+	// Filter jitter
+	if (diff > IGNORE_CHANGE_BELOW_USEC) {
+		if (state) {
+			printf("Falling\n");
+		}
+		else {
+			printf("Rising\n");
+		}
+
+		state = !state;
+	}
+
+	last_change = now;
+}
+
+int main(void) {
+	// Init
+	wiringPiSetup();
+
+	// Set pin to output in case it's not
+	pinMode(PIN, OUTPUT);
+
+	// Time now
+	gettimeofday(&last_change, NULL);
+
+	// Bind to interrupt
+	wiringPiISR(PIN, INT_EDGE_BOTH, &handle);
+
+	// Get initial state of pin
+	state = digitalRead(PIN);
+
+	if (state) {
+		printf("Started! Initial state is on\n");
+	}
+	else {
+		printf("Started! Initial state is off\n");
+	}
+
+	// Waste time but not CPU
+	for (;;) {
+		sleep(1);
 	}
 }
