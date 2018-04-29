32dc8bd34ddf33406260eba40c931c2f5e51143c
Iago <iagosimoes92@gmail.com>
2018-04-29 19:35:13 -0300

    Update quad.c

--- quad.c
+++ quad.c
@@ -21,8 +21,8 @@ int main()
     {
     system("echo 0=1000us > /dev/servoblaster");//go to 0 degree      
     ms_update();
-    printf("%d",i);
-    //printf("yaw = %2.1f\tpitch = %2.1f\troll = %2.1f\n",ypr[YAW], ypr[PITCH],ypr[ROLL]);
+    //printf("%d",i);
+    printf("yaw = %2.1f\tpitch = %2.1f\troll = %2.1f\n",ypr[YAW], ypr[PITCH],ypr[ROLL]);
     }
     return 0;
 }
