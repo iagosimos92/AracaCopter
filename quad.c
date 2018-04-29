856249f87d85238bf20ebe088a80e00238bd5f83
Iago <iagosimoes92@gmail.com>
2018-04-29 18:07:27 -0300

    Update quad.c

--- quad.c
+++ quad.c
@@ -1,31 +1,32 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <wiringPi.h>
 
 #include "MotionSensor.h"
 
+int i=0;
+void myInterrupt(void) {i++;}
+
 int main()
 {
+   
+   wiringPiSetup();
+	wiringPiISR (0, INT_EDGE_FALLING, &myInterrupt) ;
+   
    ms_open();
    // run servo program
     //system("sudo /AracaCopter/ServoBlaster/user/servod --pcm");
     printf("Init\n");
+    
    while(1)
     {
-    system("echo 0=1000us > /dev/servoblaster");//go to 0 degree
-    sleep(1);
-
-    system("echo 0=1500us > /dev/servoblaster");//go to 90 degree
-    sleep(1);
-
-    system("echo 0=2000us > /dev/servoblaster");//go to 180 degree
-    sleep(1);
-      
-    system("echo 0=1500us > /dev/servoblaster");//go to 90 degree
-    sleep(1);
+    //system("echo 0=1000us > /dev/servoblaster");//go to 0 degree
+    //sleep(1);
       
     ms_update();
-    printf("yaw = %2.1f\tpitch = %2.1f\troll = %2.1f\n",ypr[YAW], ypr[PITCH],ypr[ROLL]);
+    printf("%d",i);
+   // printf("yaw = %2.1f\tpitch = %2.1f\troll = %2.1f\n",ypr[YAW], ypr[PITCH],ypr[ROLL]);
     }
     return 0;
 }
