f6e01799f87236e92bd52f2fe95d29c679d7510a
Iago <iagosimoes92@gmail.com>
2018-04-29 18:56:41 -0300

    Update quad.c

--- quad.c
+++ quad.c
@@ -15,18 +15,14 @@ int main()
 	wiringPiISR (0, INT_EDGE_FALLING, &myInterrupt) ;
    
    ms_open();
-   // run servo program
-    //system("sudo /AracaCopter/ServoBlaster/user/servod --pcm");
-    printf("Init\n");
+   system("sudo /AracaCopter/ServoBlaster/user/servod --pcm");
     
    while(1)
     {
-    //system("echo 0=1000us > /dev/servoblaster");//go to 0 degree
-    //sleep(1);
-      
+    system("echo 0=1000us > /dev/servoblaster");//go to 0 degree      
     ms_update();
-    printf("%d",i);
-   // printf("yaw = %2.1f\tpitch = %2.1f\troll = %2.1f\n",ypr[YAW], ypr[PITCH],ypr[ROLL]);
+   // printf("%d",i);
+    printf("yaw = %2.1f\tpitch = %2.1f\troll = %2.1f\n",ypr[YAW], ypr[PITCH],ypr[ROLL]);
     }
     return 0;
 }
