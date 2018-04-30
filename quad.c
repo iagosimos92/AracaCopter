53e216ab7bc26ac522a4373ce9d9b6d3f6f4d70f
Iago <iagosimoes92@gmail.com>
2018-04-30 15:09:21 -0300

    Update quad.c

--- quad.c
+++ quad.c
@@ -9,14 +9,14 @@ volatile  int ch1=0;
 struct timeval last_change;
 
 void falling1(void) {
-   wiringPiISR (0, INT_EDGE_RISING, &rising1);
+   wiringPiISR (0, INT_EDGE_RISING, rising1);
    struct timeval now;
    gettimeofday(&now, NULL);
    ch1 = now.tv_usec - last_change.tv_usec;   
  }
 
 void rising1(void) {
-   wiringPiISR (0, INT_EDGE_FALLING, falling1);
+   wiringPiISR (0, INT_EDGE_FALLING, &falling1);
    gettimeofday(&last_change, NULL);
  }
 
