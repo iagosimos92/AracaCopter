b86f83b267c65ab4d5626a04fa4343d09621703f
Iago <iagosimoes92@gmail.com>
2018-04-30 15:20:00 -0300

    Update quad.c

--- quad.c
+++ quad.c
@@ -4,23 +4,23 @@
 #include <sys/time.h>
 #include <wiringPi.h>
 #include "MotionSensor.h"
-
+int i=0;
 volatile  int ch1=0;
 struct timeval last_change;
 
 
-
+void falling1(void) {
+   struct timeval now;
+   gettimeofday(&now, NULL);
+   ch1 = now.tv_usec - last_change.tv_usec;   
+   i=3;
+ }
 void rising1(void) {
    wiringPiISR (0, INT_EDGE_FALLING, &falling1);
    gettimeofday(&last_change, NULL);
  }
 
-void falling1(void) {
-   wiringPiISR (0, INT_EDGE_RISING, &rising1);
-   struct timeval now;
-   gettimeofday(&now, NULL);
-   ch1 = now.tv_usec - last_change.tv_usec;   
- }
+
 
 int main()
 { 
@@ -29,12 +29,16 @@ int main()
    system("sudo /AracaCopter/ServoBlaster/user/servod --pcm");
    ms_open();
    
-   while(1)
-    {
-    system("echo 0=1000us > /dev/servoblaster");//go to 0 degree      
-    ms_update();
-    printf("CH1 : %d     ",ch1);
-    //printf("yaw = %2.1f\tpitch = %2.1f\troll = %2.1f\n",ypr[YAW], ypr[PITCH],ypr[ROLL]);
+   while(1){
+     if(i==3){
+       i=0;
+       wiringPiISR (0, INT_EDGE_RISING, &rising1);
+       printf("CH1 : %d ",ch1); 
+     }
+     system("echo 0=1000us > /dev/servoblaster");//go to 0 degree      
+     ms_update();
+
+       //printf("yaw = %2.1f\tpitch = %2.1f\troll = %2.1f\n",ypr[YAW], ypr[PITCH],ypr[ROLL]);
     }
     return 0;
 }
