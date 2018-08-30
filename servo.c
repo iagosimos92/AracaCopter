0e2c80c652511373532d1f2ca9dd920a406b3b82
Iago <iagosimoes92@gmail.com>
2018-08-30 18:35:03 -0300

    Update servo.c

--- servo.c
+++ servo.c
@@ -7,15 +7,16 @@ int main()
    // run servo program
     system("sudo ~/AracaCopter/ServoBlaster/user/servod --pcm");
     printf("Init\n");
+   int x=1000;
    while(1)
     {
-    system("echo 0=1000us > /dev/servoblaster");//go to 0 degree
+    system("echo 0=%dus > /dev/servoblaster",x);//go to 0 degree
     sleep(1);
-
-    system("echo 0=1500us > /dev/servoblaster");//go to 90 degree
+    x=1500;
+    system("echo 0=%dus > /dev/servoblaster",x);//go to 90 degree
     sleep(1);
-
-    system("echo 0=2000us > /dev/servoblaster");//go to 180 degree
+    x=2000;
+    system("echo 0=%dus > /dev/servoblaster",x);//go to 180 degree
     sleep(1);
     }
     return 0;
