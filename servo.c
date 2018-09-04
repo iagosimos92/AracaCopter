7477656e966a82ce122ba168d44753a5f1347d08
Iago <iagosimoes92@gmail.com>
2018-09-04 10:25:19 -0300

    Update servo.c

--- servo.c
+++ servo.c
@@ -1,21 +1,26 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
-
+#include <string.h> //necessário para strcpy
 int main()
 {
    // run servo program
-    system("sudo ~/AracaCopter/ServoBlaster/user/servod --pcm");
-    printf("Init\n");
+   system("sudo ~/AracaCopter/ServoBlaster/user/servod --pcm");
+   printf("Init\n");
    int x=1500;
-   char string[50];
+   char str1[50];
+   char str2[4];
+   char str3[22];
+
    while(1)
     {
-    string +="echo 0=";
-    string +=x;
-    string +="us > /dev/servoblaster";
-    string +='\0' 
-    system(string);//go to 90 degree
+    str1="echo 0=";
+    str2="1500";
+    str3="us > /dev/servoblaster";
+    strncat(str1, str2, 4);
+    strncat(str1, str3, 22);
+    printf("string= %s\n",str1);
+    //system(str1);//go to 90 degree
     sleep(1);
 
     }
